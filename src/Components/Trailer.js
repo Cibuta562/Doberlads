c03291ae170750bb6ff64e6b6d5be9ca54da6758
Bogdan Gabriel Lazar <B.G.Lazar@student.tudelft.nl>
2022-03-26 20:11:32 +0100

    Links to socials

--- src/Components/Trailer.js
+++ src/Components/Trailer.js
@@ -5,12 +5,16 @@ import './Trailer.css'
 
 const Trailer = () => {
     return (
-        <div className='trailer' id="bg-video">
+        <div className='trailer'>
             <h1 className='titluTrailer'>DOBERLADS</h1>
-            <video loop autoPlay>
-                <source src={DobTrailer} type="video/mp4"/>
-            </video>
-            <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
+            <div className="box">
+                <div className='trailer' id="bg-video">
+                    <video loop autoPlay>
+                        <source src={DobTrailer} type="video/mp4"/>
+                    </video>
+                    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
+                </div>
+            </div>
         </div>
     )
 }
