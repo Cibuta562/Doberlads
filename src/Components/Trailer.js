97942eee0d079f6d745d0fb42a72edc80983f4eb
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-03-30 14:42:34 +0300

    Logo icon

--- src/Components/Trailer.js
+++ src/Components/Trailer.js
@@ -3,9 +3,11 @@ import DobTrailer from '../assets/DobTrailer.mp4'
 import './Trailer.css'
 
 /*
+<div className='trailer' id="bg-video">
  <video loop autoPlay muted>
                         <source src={DobTrailer} type="video/mp4"/>
                     </video>
+
  */
 
 const Trailer = () => {
@@ -13,10 +15,9 @@ const Trailer = () => {
         <div className='trailer'>
             <h1 className='titluTrailer'>DOBERLADS</h1>
             <div className="box">
-                <div className='trailer' id="bg-video">
 
                     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
-                </div>
+
             </div>
         </div>
     )
