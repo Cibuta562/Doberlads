37282d3f62537d7a980cd0bcc709e463925d17bd
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-04-17 20:16:55 +0300

    Mint page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from "./Components/Header";
-import RoadMap from "./Components/RoadMap";
 import Trailer from "./Components/Trailer.js";
 import Footer from "./Components/Footer";
-import Metamask from "./Components/Metamask";
 import Team from "./Components/Team"
 import Mint from './Components/Mint'
 import "animate.css/animate.min.css";
 import Faq from "./Components/Faq"
 import HeaderMobile from "./Components/HeaderMobile";
 import About from "./Components/About"
-import { ethers, BigNumber } from "ethers";
 import React, { useEffect, useState} from "react";
-import mint from "./mintAbi.json"
 import FaqJr from "./Components/FaqJr";
 import Timer from "./Components/Timer";
 import AbtTeam from './Components/AbtTeam'
@@ -21,16 +16,12 @@ import "./Fonts/Roboto/Roboto-Bold.ttf";
 import "./Fonts/BebasNeue/BebasNeue/TTF/BebasNeue-Bold.ttf";
 import "./Fonts/Archive/Archive/Commercial/OTF/Archive-Regular.otf";
 import "./Fonts/Roboto/Roboto-Black.ttf";
-import ScrollAnimation from 'react-animate-on-scroll';
-import background from './assets/background.jpg'
-import App2 from "./App2";
 import CookieConsent from "react-cookie-consent";
 import ScrollAnimate from "react-scroll-fade-animation";
 import ReactGA from 'react-ga';
 import NewFaqAcc from "./Components/NewFaqAcc";
 import CTA from "./Components/CTA";
 import Transition from "./Components/Transition";
-const mintAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 ReactGA.initialize('UA-223827648-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
--- src/Components/Mint.js
+++ src/Components/Mint.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import './Mint.css'
-import { useEffect, useState} from "react";
 import Footer from "./Footer";
 import LinksFooter from "./LinksFooter";
 import HeaderMobile from "./HeaderMobile";
 import nftBg from "../assets/mint_pic-removebg-preview.png";
+import { FC, useEffect, useState } from "react";
+
+
+
+
 
 const Mint = () => {
 
 
-//MINTAT COAIE//
 
     const [mintAmount, setMintAmount] = useState(1);
     const [sum, setSum] = useState(2);
-    const [text, setText] = useState([" DOBERLAD"]);
+    const [text, setText] = useState([" DOB"]);
     const [meta, setMetaMask] = useState("Please connect your wallet");
     const [href, sethref] = useState("https://metamask.io/download/");
+    const [nrMint, setNrMint] = useState(0);
 
 
 
     function numberOfMintsDecrease() {
 
         if (mintAmount === 2) {
-            setText("DOBERLAD");
+            setText("DOB");
         }
         if(mintAmount <= 1){
             setMintAmount(1);
@@ -37,7 +41,7 @@ const Mint = () => {
     function numberOfMintsIncrease() {
 
         if (mintAmount === 1) {
-            setText("DOBERLADS");
+            setText("DOBS");
         }
         if (mintAmount >= 2) {
             setMintAmount(2);
@@ -79,9 +83,13 @@ const Mint = () => {
             <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet"/>
             <HeaderMobile/>
             <div className="mint-content">
+                <div className="mint-h">
+                    <h2 className="heading-h">Become a doberlads holder</h2>
+                    <p className="heading-p">By holding a Doberlad you become aligible to do challanges in our app and by doing so you will get rewarded with our token</p>
+                </div>
                 <section>
 
-                        <div className="background-img">
+                        <div className="background-img" >
                             <div className="box-mint">
                                 <span></span>
                                 <span></span>
@@ -91,14 +99,14 @@ const Mint = () => {
                             <div className="box-mint-text">
                                 <div className="content-mint" style={{alignContent: "center"}}>
                                     <center>
-                                        <h2>Chose how many NFTS you want to mint</h2>
-                                        <div>
+                                        <h2>Chose how many DOBERLADS you want to mint</h2>
+                                        <div style={{marginTop: "20px"}}>
                                             <button className='mintButton1-mint-sign' onClick={numberOfMintsDecrease}>
                                                 -
                                             </button>
-                                            <span className="mintButton1-mint-amount" style={{marginRight: "8px", marginLeft: "8px"}}>
+                                            <button className="mintButton1-mint-amount" style={{marginRight: "8px", marginLeft: "8px"}}>
                                                 {mintAmount} {text}
-                                            </span>
+                                            </button>
                                             <button className='mintButton1-mint-sign' onClick={numberOfMintsIncrease}>
                                                 +
                                             </button>
@@ -106,14 +114,17 @@ const Mint = () => {
                                         <div className="mintButton1-mint-no-hover">
                                             {sum} SOL
                                         </div>
-                                    <button className='mintButton1-mint'>
+                                    <div className='mintButton1-mint'>
                                         MINT
-                                    </button>
+                                    </div>
                                     </center>
                                 </div>
                             </div>
                     </div>
                 </section>
+                <div className="nr-mints">
+                    <p>{nrMint}/10.000 Minted</p>
+                </div>
             </div>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet"/>
             <link href="https://fonts.googleapis.com/css2?family=Fredoka&display=swap" rel="stylesheet"/>
